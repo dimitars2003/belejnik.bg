e13c20c0d5f5fc6cfdc87dd1f28a6122550c7918
2017_dimitarsd1 <2017_dimitarsd1@LAB27-15.school.npmg.org>
2022-03-09 13:03:16 +0200

    a

---
+++ Belejnik/Belejnik/src/main/java/com/example/demo/BelejnikApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelejnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelejnikApplication.class, args);
+	}
+
+}
