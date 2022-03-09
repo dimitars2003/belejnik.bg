67388648d49db0c7b67951a5abc67e40be4d2c45
2017_dimitarsd1 <2017_dimitarsd1@LAB27-15.school.npmg.org>
2022-03-09 13:06:51 +0200

    a

--- Belejnik/Belejnik/src/main/java/com/example/demo/BelejnikApplication.java
+++ Belejnik/Belejnik/src/main/java/com/example/demo/BelejnikApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BelejnikApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BelejnikApplication.class, args);
+		SpringApplication.run(BelejnikApplication.class, args);;
 	}
 
 }
